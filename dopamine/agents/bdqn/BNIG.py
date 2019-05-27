985369bb84dc21bc3e665625d46ce6d4a53a6e76
Unknown <rasmusmunter@gmail.com>
2019-05-27 13:49:02 +0200

    Exponential forgetting

--- dopamine/agents/bdqn/BNIG.py
+++ dopamine/agents/bdqn/BNIG.py
@@ -17,7 +17,7 @@ class BNIG():
                  replay_next_state,
                  replay_buffer,
                  coef_var=1,
-                 lr=0.01):
+                 lr=1e-2):
         self.action = action
 
         # External Tensors
@@ -25,41 +25,48 @@ class BNIG():
 
         self.input_size =  int(state.get_shape()[1])
         self.coef_var = coef_var
+        self.mem = 1-lr
 
         with tf.name_scope("BNIG"):
 
-            # Variables
-            self.mean = tf.get_variable(
-                str(action)+"/parameters/mean", initializer=self.mean_initializer, trainable=False)
-            self.cov = tf.get_variable(
-                str(action)+"/parameters/cov", initializer=self.cov_initializer, trainable=False)
-            self.a = tf.get_variable(
-                str(action)+"/parameters/alpha", initializer=self.alpha_initializer, trainable=False)
-            self.b = tf.get_variable(
-                str(action)+"/parameters/beta", initializer=self.beta_initializer, trainable=False)
+            self._create_model_variables()
 
             # Graphs
             self.sample_op = self._sampler_graph(state)
             self.target_sample_op = self._sampler_graph(replay_next_state, n=self._replay.batch_size)
 
     @property
-    def mean_initializer(self):
+    def mean_prior(self):
         return tf.cast(np.zeros((self.input_size)), dtype=tf.float32)
 
     @property
-    def cov_initializer(self):
+    def cov_prior(self):
         return tf.cast(np.eye(self.input_size)*self.coef_var, dtype=tf.float32)
 
     @property
-    def alpha_initializer(self):
+    def alpha_prior(self):
         return tf.cast(1, dtype=tf.float32)
 
     @property
-    def beta_initializer(self):
+    def beta_prior(self):
         return tf.cast(1e-3, dtype=tf.float32)
 
+    def _create_model_variables(self):
+        with tf.variable_scope(str(self.action)+"/parameters/"):
+            self.mean = tf.get_variable("mean",  initializer=self.mean_prior, trainable=False)
+            self.cov  = tf.get_variable("cov",   initializer=self.cov_prior, trainable=False)
+            self.alpha= tf.get_variable("alpha", initializer=self.alpha_prior, trainable=False)
+            self.beta = tf.get_variable("beta",  initializer=self.beta_prior, trainable=False)
+
+        with tf.variable_scope(str(self.action)+"/data/"):
+            self.XTX = tf.get_variable("XTX", initializer=tf.cast(np.zeros((self.input_size, self.input_size)), dtype=tf.float32), trainable=False)
+            self.XTy = tf.get_variable("XTy", initializer=tf.cast(np.zeros((self.input_size, 1)), dtype=tf.float32), trainable=False)
+            self.n   = tf.get_variable("n",   initializer=tf.cast(0, dtype=tf.float32), trainable=False)
+            self.yTy = tf.get_variable("yTy", initializer=tf.cast(np.zeros((1,1)), dtype=tf.float32), trainable=False)
+
+
     def _sampler_graph(self, _input, n=1):
-        sigma_dist = tfd.InverseGamma(concentration=self.a, rate=self.b)
+        sigma_dist = tfd.InverseGamma(concentration=self.alpha, rate=self.beta)
         sigma = tf.sqrt(sigma_dist.sample(1))
         normal = tfd.Normal(loc=0, scale=1)
         coef = self.mean[:,None] + sigma*tf.linalg.cholesky(self.cov)@normal.sample((self.input_size,n))
@@ -82,31 +89,35 @@ class BNIG():
 
     def _build_bayes_posterior_update(self, X, y, n):
 
-            lr = 0
+            XTX = self.mem*self.XTX + tf.transpose(X)@X
+            XTy = self.mem*self.XTy + tf.transpose(X)@y
+            yTy = self.mem*self.yTy + tf.transpose(y)@y
 
-            temp = tf.linalg.inv(tf.linalg.cholesky(self.cov))
-            inv_cov = tf.transpose(X)@X + (1-lr)*tf.transpose(temp)@temp
+            n   = self.mem*self.n   + tf.cast(n, tf.float32)
 
-
-            temp2 = tf.linalg.inv(tf.linalg.cholesky(inv_cov))
-            cov = tf.transpose(temp2)@temp2
+            inv_cov = XTX + tf.linalg.inv(self.cov_prior)
+            temp = tf.linalg.inv(tf.linalg.cholesky(inv_cov))
+            cov = tf.transpose(temp)@temp
     
-            mean = cov@(tf.transpose(X)@y +
-                        (1-lr)*tf.linalg.inv(self.cov)@self.mean[:,None])
+            mean = cov@XTy
 
-            alpha = (1-lr)*self.a + \
+            alpha = self.alpha_prior + \
                 tf.cast(n/2, tf.float32)
 
-            b = (1-lr)*self.b + 0.5*\
-                tf.squeeze(tf.transpose(y)@y -
-                            tf.transpose(mean)@inv_cov@mean + 
-                            (1-lr)*tf.transpose(self.mean[:,None])@
-                            tf.linalg.inv(self.cov)@self.mean[:,None])
-            
+            beta = tf.maximum(self.beta_prior + \
+                    0.5*tf.squeeze(yTy -
+                        tf.transpose(mean)@inv_cov@mean),
+                    1e-6)
+
             tf.summary.scalar(str(self.action), alpha, family="alpha")
-            tf.summary.scalar(str(self.action), b, family="beta")
+            tf.summary.scalar(str(self.action), beta, family="beta")
             
-            return  [tf.assign(self.mean, tf.squeeze(mean)),
-                    tf.assign(self.cov, cov),
-                    tf.assign(self.a, alpha),
-                    tf.assign(self.b, b)]
+
+            return  [tf.assign(self.mean , tf.squeeze(mean)),
+                    tf.assign(self.cov  , cov),
+                    tf.assign(self.alpha, alpha),
+                    tf.assign(self.beta , beta),
+                    tf.assign(self.XTX  , XTX),
+                    tf.assign(self.XTy  , XTy),
+                    tf.assign(self.n    , n),
+                    tf.assign(self.yTy  , yTy)]
